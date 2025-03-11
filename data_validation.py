8d416a1a8cd42f551d9a8c283d4517271bd08c88
Sebastian-Lore <sebastianlorenzo392@gmail.com>
2025-03-10 19:04:50 -0500

    filled in null values
    Had to use fillna function to fill in missing values because Pandera’s nullable=False does not catch NaN values by default for string columns.

--- data_validation.py
+++ data_validation.py
@@ -2,38 +2,45 @@
 import pandas as pd
 import pandera as pa
 from pandera import Column, Check
+import os
 
 # define schema
 alarm_schema = pa.DataFrameSchema({
-    "alarm_id": Column(int),
+    "alarm_id": Column(int, nullable=False),
     "timestamp": Column(str, Check.str_matches(r"^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$"), nullable=False),
     "site_id": Column(int),
     "alarm_code": Column(str, Check.str_matches(r"^ALM\d+$"), nullable=False),
-    "severity": Column(str, Check.isin(["Critical", "Major", "Minor", "Warning"])),
-    "status": Column(str, Check.isin(["New", "Acknowledged", "Resolved"]))
+    "severity": Column(str, Check.isin(["Critical", "Major", "Minor", "Warning"]), nullable=False),
+    "status": Column(str, Check.isin(["New", "Acknowledged", "Resolved"]), nullable=False)
 })
 
 # validate CSV data
 def validate_csv(file_path):
     df = pd.read_csv(file_path)
+    file_name = os.path.basename(file_path) # to get the name of the file
+
+    # use fillna function to fill in missing values because Pandera’s nullable=False does not catch NaN values by default for string columns
+    df.fillna({"timestamp": "No string has been Entered"}, inplace=True)
+    df.fillna({"alarm_code": "No string has been Entered"}, inplace=True)
+    df.fillna({"severity": "No string has been Entered"}, inplace=True) 
+    df.fillna({"status": "No string has been Entered"}, inplace=True)
+
     try:
         alarm_schema.validate(df, lazy=True) # enable lazy validation to catch all errors at once
     except pa.errors.SchemaErrors as e:  # use "SchemaErrors" not "SchemaError"
         failure_cases = e.failure_cases
         if not failure_cases.empty:
-            print("\nValidation errors found:\n")
+            print(f"\nValidation errors found in {file_name}:\n")
             
             for _, error in failure_cases.iterrows():
                 index = error["index"]
                 failure_case = error["failure_case"]
-                
                 failing_row = df.iloc[index]
-
+                
                 for column in alarm_schema.columns:
                     if failure_case == failing_row[column]:
                         print(f"\nColumn '{column}' failed validation:")
                         print(f"  alarm_id: {failing_row['alarm_id']}")
-                        print(f"  {column}: {failing_row[column]}")
                         print(f"  Failure: {failure_case}\n")
                         break
 
